ec1c9bae749d4f6a7f0872b973f3ef26b05b1278
Usmon <usmonzaripov@gmail.com>
2023-10-08 21:43:56 +0500

    Intial

---
+++ src/main/java/com/junior/crud/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.junior.crud.repositories;
+
+import com.junior.crud.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
